992471577586660ad62789ddc93ce13b424731a0
Glitch (glitch-hello-node) <none>
2021-05-04 07:41:09 +0000

    🕍🐕 Checkpoint

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get("/",(req,res)=>{
 let secret=process.env.SECRET;
 res.send(secret);
 })
+app.get("/linux",(req,res)=>{
+res.send("linux");
 
+
+})
 
 app.listen(process.env.PORT,console.log("server is running"));
